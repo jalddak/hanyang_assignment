fa4d61317d1f45a6983fe15c25555db131817df4
jalddak <jalddak98@gmail.com>
2022-04-22 23:06:52 +0900

    04-22

--- 2022_ite4005_2017030464/decision_tree/dt.py
+++ 2022_ite4005_2017030464/decision_tree/dt.py
@@ -1,16 +1,55 @@
 import sys
+import csv
+
+import gini
 
 def main():
     # 입력 받아오는 부분
     args = sys.argv[1:]
-    min_sup = int(args[0])
     try:
-        input = open(args[1], 'r', encoding='utf-8')
+        train_file = open(args[0], 'r', encoding='utf-8')
     except FileNotFoundError:
-        print("*** input 파일이 없습니다. ***\n")
+        print("*** train 파일이 없습니다. ***\n")
         return 0
-    output = open(args[2], 'w', encoding='utf-8')
+    try:
+        test_file = open(args[1], 'r', encoding='utf-8')
+    except FileNotFoundError:
+        print("*** test 파일이 없습니다. ***\n")
+        return 0
+    result_file = open(args[2], 'w', encoding='utf-8')
 
+    training_rdr = csv.reader(train_file, delimiter='\t')
+    test_rdr = csv.reader(test_file, delimiter='\t')
+
+    training_data_set = []
+    for line in training_rdr:
+        training_data_set.append(line)
+    training_attributes = training_data_set.pop(0)
+    training_data_dic = []
+    for data in training_data_set:
+        dic = {}
+        for i in range(len(data)):
+            dic[training_attributes[i]] = data[i]
+        training_data_dic.append(dic)
+
+    test_data_set = []
+    for line in test_rdr:
+        test_data_set.append(line)
+    test_attributes = test_data_set.pop(0)
+    test_data_dic = []
+    for data in test_data_set:
+        dic = {}
+        for i in range(len(data)):
+            dic[test_attributes[i]] = data[i]
+        test_data_dic.append(dic)
+
+    decision_tree = []
+    gini.calc(training_data_dic)
+    gini.compare(training_data_dic, training_attributes)
+
+    train_file.close()
+    test_file.close()
+    result_file.close()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
---
+++ 2022_ite4005_2017030464/decision_tree/gini.py
@@ -0,0 +1,27 @@
+def calc(data_set_dic):
+    data_set = []
+    for data_dic in data_set_dic:
+        data_set.append(list(data_dic.values()))
+    dic = {}
+    data_size = 0
+    for data in data_set:
+        if data[len(data)-1] in dic:
+            dic[data[len(data)-1]] += 1
+        else:
+            dic[data[len(data) - 1]] = 1
+        data_size += 1
+
+    value = 1
+    for i in range(len(dic)):
+        p = list(dic.values())[i]/data_size
+        p_squared = p ** 2
+        value -= p_squared
+
+def compare(data_set_dic, attributes):
+    data_set = []
+    for data_dic in data_set_dic:
+        data_set.append(list(data_dic.values()))
+    data_size = len(data_set)
+
+    compare_list = []
+    for i in range(attributes - 1):
