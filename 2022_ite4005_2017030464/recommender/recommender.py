72c6ca3702161b168da3f65426b194dca8f254c9
jalddak <jalddak98@gmail.com>
2022-06-11 05:27:11 +0900

    recommender_test

--- 2022_ite4005_2017030464/recommender/recommender.py
+++ 2022_ite4005_2017030464/recommender/recommender.py
@@ -21,25 +21,12 @@ def get_rmse(R, P, Q, non_zeros):
     return rmse
 
 
-def main():
-    # 입력 받아오는 부분
-    args = sys.argv[1:]
-    base_name = args[0]
-    test_name = args[1]
-
-    base_data = pd.read_csv("./data/" + base_name, delimiter='\t', names=['user_id', 'item_id', 'rating', 'time_stamp'])
-    test_data = pd.read_csv("./data/" + test_name, delimiter='\t', names=['user_id', 'item_id', 'rating', 'time_stamp'])
-    del base_data['time_stamp']
-    del test_data['time_stamp']
-    test_data['rating'] = 0
-
-    data = pd.merge(base_data, test_data, how='outer', on=['user_id', 'item_id', 'rating'])
-
-    data_rating = data.pivot_table('rating', index='user_id', columns='item_id').replace(0, np.nan)
-
-    # 원본 행렬 R 생성, 분해 행렬 P와 Q 초기화, 잠재요인 차원 K는 3 설정.
-    R = data_rating.values
+def after_matrix_factorization(table):
+    R = table.values
     num_users, num_items = R.shape
+    # K의 값이 클수록 실제 base_data에 있는 실제값과 SGD 방법을 이용한 MF 방식의
+    # 예측값의 유사도는 비슷하였으나, PA4.exe 의 rmse는 오히려 더 떨어졌다.
+    # K 값이 클 수록 프로그램 시간이 오래걸리는데, 굳이 크게 해줄 이유가 없어보인다.
     K = 3
 
     # P와 Q 매트릭스의 크기를 지정하고 정규분포를 가진 random한 값으로 입력합니다.
@@ -54,9 +41,10 @@ def main():
     learning_rate = 0.01
     r_lambda = 0.01
 
-    before_rmse = 0
     # SGD 기법으로 P와 Q 매트릭스를 계속 업데이트.
-    while True:
+    count = 0
+    before_rmse = 0
+    while count < 3:
         step += 1
         for i, j, r in non_zeros:
             # 실제 값과 예측 값의 차이인 오류 값 구함
@@ -66,25 +54,79 @@ def main():
             Q[j, :] = Q[j, :] + learning_rate * (eij * P[i, :] - r_lambda * Q[j, :])
 
         rmse = get_rmse(R, P, Q, non_zeros)
+        # 처음엔 그냥 한번만이라도 rmse 가 전과 0.001 이하로 차이나면, 비슷하다고 가정하고 그냥 멈췄는데,
+        # zero_injection을 수행하는 과정에서 초반에 0.001 이하로 차이나는 경우가 생겨서 바로 끝나버려 제대로 된 예측을 시작하기전에
+        # 끝나버려서 기회를 3번정도 더 주는 것으로 코드 수정함.
         if before_rmse != 0 and before_rmse - rmse < 0.001:
-            break
+            count += 1
         before_rmse = rmse
     pred_matrix = np.dot(P, Q.T)
 
-    pred_matrix_int = np.round(pred_matrix, 0).astype(int)
-    result = pd.DataFrame(pred_matrix_int, columns=data_rating.columns, index=data_rating.index)
+    return pred_matrix
 
+
+def main():
+    # 입력 받아오는 부분
+    args = sys.argv[1:]
+    base_name = args[0]
+    test_name = args[1]
+
+    # zero_injection 의 경우도 생각해보려했으나, 너무 오래걸리고, PA4.exe 의 rmse 결과 또한 더 나빠져서 그냥 안했다.
+    # (주석 처리된 코드는 모두 zero_injection 관련 코드이다. 혹시몰라서 주석처리로 남겨뒀다.)
+    base_data = pd.read_csv("./data/" + base_name, delimiter='\t', names=['user_id', 'item_id', 'rating', 'time_stamp'])
+    test_data = pd.read_csv("./data/" + test_name, delimiter='\t', names=['user_id', 'item_id', 'rating', 'time_stamp'])
+    # test_data2 = pd.read_csv("./data/" + test_name, delimiter='\t', names=['user_id', 'item_id', 'rating', 'time_stamp'])
+    del base_data['time_stamp']
+    del test_data['time_stamp']
+    test_data['rating'] = 0
+    # del test_data2['time_stamp']
+    # del test_data2['rating']
+
+    data = pd.merge(base_data, test_data, how='outer', on=['user_id', 'item_id', 'rating'])
+    # zero_one_data = pd.merge(base_data, test_data2, how='outer', on=['user_id', 'item_id'])
+
+    rating_table = data.pivot_table('rating', index='user_id', columns='item_id').replace(0, np.nan)
+    # ratings = zero_one_data.groupby('rating').count().iloc[:, :1] / zero_one_data.count().user_id
+    # zero_one_table = data.pivot_table('rating', index='user_id', columns='item_id').replace(ratings.index, 1)
+    # zero_one_table = zero_one_table.fillna(0)
+
+    # print("zero_injection_start")
+    # zero_injection_pred_matrix = after_matrix_factorization(zero_one_table)
+    # zero_injection_pred_matrix = np.round(zero_injection_pred_matrix, 3)
+    # zi_result = pd.DataFrame(zero_injection_pred_matrix, columns=rating_table.columns, index=rating_table.index)
+    # print("zero_injection_end")
+    #
+    # print("rating_table_change_start")
+    # for i in rating_table.index:
+    #     for j in rating_table.columns:
+    #         if zi_result.loc[i, j] < 0.5:
+    #             rating_table.loc[i, j] = 1
+    # print("rating_table_change_end")
+
+    print("cf_start")
+    cf_pred_matrix = after_matrix_factorization(rating_table)
+    cf_pred_matrix = np.round(cf_pred_matrix, 0).astype(int)
+    cf_result = pd.DataFrame(cf_pred_matrix, columns=rating_table.columns, index=rating_table.index)
+    print("cf_end")
+
+    print("output_write_start")
     output = open('./data/' + base_name + "_prediction.txt", 'w', encoding='utf-8')
     for t in test_data.values:
-        predict = result.loc[t[0], t[1]]
+        predict = cf_result.loc[t[0], t[1]]
         if predict > 5:
             predict = 5
         elif predict < 1 or predict == np.nan:
-            predict = 1
+            # else의 경우는 아예 새로운 데이터 였을 경우라고 생각했다. 그럴 경우, 거의 모든 값이 0보다 작은 값인 것 같다.
+            if (cf_result[t[1]].sum() / len(cf_result)) > 0.5:
+                predict = 1
+            else:
+                select = base_data.loc[base_data['user_id'] == t[0]]
+                predict = int((select['rating'].sum() / len(select)).round())
         output.write(str(t[0]) + '\t')
         output.write(str(t[1]) + '\t')
         output.write(str(predict) + '\n')
     output.close()
+    print("output_write_end")
 
 
 if __name__ == '__main__':
